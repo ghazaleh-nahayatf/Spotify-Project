23cc801dc83b23a64110c0106c0c24296f49af0b
Ghazaleh <ghazaleh.nahayat.f@gmail.com>
2026-07-15 11:53:36 +0330

    Add Account entity

---
+++ account.cpp
@@ -0,0 +1,25 @@
+#include "account.h"
+
+Account::Account(){}
+Account::Account(string fullName, string userName, string biography,string ID, string role,string password)
+    : fullName(fullName),
+    userName(userName),
+    biography(biography),
+    ID(ID),
+    role(role),
+    password(password)
+{
+}
+
+string Account :: getUserName()const
+{
+    return userName;
+}
+string Account :: getPassword()const
+{
+    return password;
+}
+void Account :: setBiography(const string& biography)
+{
+    this->biography=biography;
+}
---
+++ account.h
@@ -0,0 +1,34 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+#include <string>
+
+using namespace std;
+
+class Account
+{
+protected:
+    string fullName;
+    string userName;
+    string biography;
+    string ID;
+    string role;
+    string password;
+public:
+    Account();
+    Account(string fullName,
+            string userName,
+            string biography,
+            string ID,
+            string role,
+            string password);
+
+    string getUserName() const;
+    string getPassword() const;
+    void setBiography(const string& biography);
+
+    virtual void showProfile()=0;
+    virtual ~Account(){}
+
+};
+
+#endif // ACCOUNT_H
